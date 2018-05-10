19965f2fd10fc90e144ac0d4cba797229d38e271
adumbidiot <nathaniel.daniel23@outlook.com>
2018-05-10 14:58:29 -0700

    Finished basic IRArray implementation

--- FC51-IRArray.h
+++ FC51-IRArray.h
@@ -1,31 +1,46 @@
 #ifndef IRARRAY_H
 #define IRARRAY_H
 #include "Arduino.h"
-class IRArray{
-	public:
-	
-	IRArray(int portA, int portB, int portC, int portD){
-		sensors[0] = portA;
-		sensors[1] = portB;
-		sensors[2] = portC;
-		sensors[3] = portD;
-		for(int i = 0; i != SENSOR_COUNT; i++){
-			pinMode(sensors[i], INPUT);
+namespace FRC6644 {
+	template <int N>
+	class FC51_IRArray{
+		public:
+		FC51_IRArray(std::initializer_list<int> ports){
+			std::initializer_list<int>::iterator it;
+			auto list_pos = ports.begin();
+			for(int i = 0; i != size; i++, list_pos++){
+				sensors[i] = *list_pos;
+				pinMode(sensors[i], INPUT);
+			}
 		}
-	}
-	int getDistances(){
-		return distances;
-	}
-	void readSensors(){
-		distances = 0;
-		for(int i = 0; i != SENSOR_COUNT; i++){
-				distances |= ((digitalRead(sensors[i]) == LOW) << i);
+		
+		int getDistances(){
+			return distances;
 		}
-	}
+		
+		int getSize(){
+			return size;
+		}
+		
+		void readSensors(){
+			distances = 0;
+			for(int i = 0; i != size; i++){
+					distances |= ((digitalRead(sensors[i]) == LOW) << i);
+			}
+		}
+		void printFormattedIRData(FC51_IRArray array){
+			int data = array.getDistances();
+			for(int i = 0; i != array.getSize(); i++){
+				int value = (data & (1 << i)) >> i;
+				Serial.println("Sensor " + String(i) + " :" + " " + String(value));
+			}
+			Serial.println();
+		}
+		private:
+			int sensors[N];
+			int distances;
+			const int size = N;
+	};
 	
-	private:
-		int sensors[4];
-		int distances;
-		static constexpr int SENSOR_COUNT = 4;
 };
 #endif
